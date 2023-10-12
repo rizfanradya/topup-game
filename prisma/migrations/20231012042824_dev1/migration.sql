b3ab3f6ad859f649bd45946485cd7914d314a7b1
rizfanradya <rizfankusuma@gmail.com>
2023-10-12 11:36:16 +0700

    up

--- prisma/migrations/20231012042824_dev1/migration.sql
+++ prisma/migrations/20231012042824_dev1/migration.sql
@@ -3,8 +3,6 @@ CREATE TABLE `admin` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
